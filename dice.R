bcdea36f8cfb7e60d6c0555c2db54d81ff2414ec
Jim Leach <jimleach@kpmg.com>
2019-08-30 16:33:01 -0400

    Set up dice, sides, and simple die-roller function

--- dice.R
+++ dice.R
@@ -1,15 +1,64 @@
-purple <- c("t", "f", "ft", "t", "", "tt", "ff", "t")
-green <- c("s", "a", "sa", "ss", "a", "s", "aa", "")
-yellow <- c("aa", "a", "aa", "tr", "s", "sa", "s", "sa", "ss", "sa", "ss", "")
-red <- c("tt", "t", "tt", "t", "tf", "f", "tf", "f", "ff", "d", "ff", "")
-
+# Set up dice sides
 sides <- list(
-    "d" = 8,
-    "a" = 8,
-    "c" = 12,
-    "p" = 12,
+    # Positive dice - boost, ability, proficiency
     "b" = 6,
-    "s" = 6
+    "a" = 8,
+    "p" = 12,
+    # Negative dice - setback, difficulty, challenge
+    "s" = 6,
+    "d" = 8,
+    "c" = 12
 )
+
+# Define positive dice ---------------------------------------------------------
+successes <- list(
+    # Positive dice only - boost, ability, proficiency
+    "b" = c(0, 0 , 1, 1, 0, 0),
+    "a" = c(0, 1, 1, 2, 0, 0, 1, 0),
+    "p" = c(0, 1, 1, 2, 2, 0, 1, 1, 1, 0, 0, 1)
+)
+
+advantages <- list(
+    # Positive dice only - boost, ability, proficiency
+    "b" = c(0, 0, 0, 1, 2, 1),
+    "a" = c(0, 0, 0, 0, 1, 1, 1, 2),
+    "p" = c(0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 0)
+)
+
+triumphs <- list("p" = c(rep(0, 11), 1))
+
+# Define positive dice ---------------------------------------------------------
+failures <- list(
+    "s" = c(0, 0 , 1, 1, 0, 0),
+    "d" = c(0, 1, 2, 0, 0, 0, 0, 1),
+    "c" = c(0, 1, 1, 2, 2, 0, 0, 1, 1, 0, 0, 1)
+)
+
+threat <- list(
+    "s" = c(0, 0 , 0, 0, 1, 1),
+    "d" = c(0, 0, 0, 1, 1, 1, 2, 1),
+    "c" = c(rep(0, 5), rep(1, 4), 2, 2, 0)
+)
+
+despair <- list("c" = c(rep(0, 11), 1))
+
+
+# Functions ---------------------------------------------------------------
+roll_die <- function(die) {
+    side <- sample(sides[[die]], 1)
+    c(
+        # Positive results
+        .suc = successes[[die]][[side]],
+        .adv = advantages[[die]][[side]],
+        .tri = triumphs[[die]][[side]],
+
+        # Negative results
+        .fail = failures[[die]][[side]],
+        .thre = threat[[die]][[side]],
+        .desp = despair[[die]][[side]]
+    )
+}
+
+
 
 
