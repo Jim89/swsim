9fde46ffba46ebd8a2ec89624511cc487b7c7c2f
Jim Leach <jimleach@kpmg.com>
2019-08-30 16:01:59 -0400

    Initial commit

---
+++ dice.R
@@ -0,0 +1,15 @@
+purple <- c("t", "f", "ft", "t", "", "tt", "ff", "t")
+green <- c("s", "a", "sa", "ss", "a", "s", "aa", "")
+yellow <- c("aa", "a", "aa", "tr", "s", "sa", "s", "sa", "ss", "sa", "ss", "")
+red <- c("tt", "t", "tt", "t", "tf", "f", "tf", "f", "ff", "d", "ff", "")
+
+sides <- list(
+    "d" = 8,
+    "a" = 8,
+    "c" = 12,
+    "p" = 12,
+    "b" = 6,
+    "s" = 6
+)
+
+
